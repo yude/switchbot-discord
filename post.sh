364c3e438957c97389e5cd9d74df707245a58626
yudejp <i@yude.jp>
2021-07-02 16:50:53 +0900

    Update

--- post.sh
+++ post.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-json=$(/usr/bin/curl -s -X GET "https://api.switch-bot.com/v1.0/devices/${secrets.DEVICE_ID}/status" -H "Authorization: ${secrets.TOKEN}")
+
+json=$(/usr/bin/curl -s -X GET "https://api.switch-bot.com/v1.0/devices/${DEVICE_ID}/status" -H "Authorization: ${TOKEN}")
 humid=$(echo $json | jq '.body.humidity')
 temp=$(echo $json | jq '.body.temperature')
 
 content="[$(date "+%Y/%m/%d %H:%M:%S")] 気温: ${temp}, 湿度: ${humid}"
-curl -H "Content-Type: application/json" -X POST -d '{"username": "Switchbot", "content": "'"$content"'"}' ${secrets.URL}
+curl -H "Content-Type: application/json" -X POST -d '{"username": "Switchbot", "content": "'"$content"'"}' $URL
