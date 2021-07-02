0d31e324eaa1ac275926675dc65b41a1f6110a34
yudejp <i@yude.jp>
2021-07-02 16:56:08 +0900

    Set timezone in env

--- post.sh
+++ post.sh
@@ -4,7 +4,7 @@
 json=$(/usr/bin/curl -s -X GET "https://api.switch-bot.com/v1.0/devices/${DEVICE_ID}/status" -H "Authorization: ${TOKEN}")
 humid=$(echo $json | jq '.body.humidity')
 temp=$(echo $json | jq '.body.temperature')
-date=$(TZ=-9 date "+%Y/%m/%d %H:%M:%S")
+date=$(date "+%Y/%m/%d %H:%M:%S")
 
 content="気温: ${temp} ℃, 湿度: ${humid} %"
 curl -H "Content-Type: application/json" -X POST -d '{"username": "'"$date"'", "content": "'"$content"'"}' $URL
