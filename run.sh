764bcf02953be912ad0731f1d3f01d6007d8fed3
yude <i@yude.jp>
2022-04-06 11:46:21 +0900

    Remove embed graph

--- run.sh
+++ run.sh
@@ -8,5 +8,4 @@ temp=$(echo $json | jq '.body.temperature')
 date=$(date "+%Y/%m/%d %H:%M:%S")
 
 content="気温: ${temp} ℃, 湿度: ${humid} %"
-embed="https://mackerel.io/embed/public/embed/4mVIU29WCRVZgHUxQPzWsfXo953uxiAegbjDb83hTQ7szesCunwdpVkBIzhnLc9i.png?period=15m"
-curl -H "Content-Type: application/json" -X POST -d '{"username": "'"$date"'", "content": "'"$content\r$embed"'"}' $WEBHOOK_URL
+curl -H "Content-Type: application/json" -X POST -d '{"username": "'"$date"'", "content": "'"$content"'"}' $WEBHOOK_URL
